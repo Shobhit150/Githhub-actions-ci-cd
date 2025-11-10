7e8510729ab27f8eeaad7e4be6d346d329fbcff6
Shobhit150 <vshobhit150@gmail.com>
2025-11-10 10:46:11 +0530

    lets check

---
+++ function/function.go
@@ -0,0 +1,8 @@
+package function
+
+func Sum(a int, b int) int {
+	return a+b
+}
+func IsEven(a int) bool {
+	return a%2==0
+}
--- testing/main_test.go
+++ testing/main_test.go
@@ -1,9 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"github.com/Shobhit150/Githhub-actions-ci-cd/function"
+)
 
-func TestDummy(t *testing.T)  {
-	if 2+2 == 4 {
-		t.Fail()
+func TestDummy(t *testing.T) {
+	t.Log("Starting TestDummy")
+	got := function.Sum(3, 2)
+	t.Logf("Sum(3, 2) returned %d", got)
+	
+	if got != 5 {
+		t.Fatalf("Expected 5, got %d", got)
 	}
+
+
+	t.Log("TestDummy completed successfully")
 }
